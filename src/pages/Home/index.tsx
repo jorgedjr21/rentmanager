da87a9dc6f027e59f934e93f3bbcaf596eb56f5f
Jorge Junior <jorgedjr21@gmail.com>
2020-06-06 23:27:59 -0300

    update Home with some infos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,27 +1,52 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Header from '../../components/Header';
+import InfoCard from '../../components/InfoCard';
 import {Button, Container, Jumbotron, Row, Col} from 'react-bootstrap';
+
+import datesService from '../../services/datesService';
 import './styles.css'
 
 const Home = () => {
+
+  const [monthName, setMonthName] = useState('');
+  const [dateToPayment, setDateToPayment] = useState(0);
+
+  useEffect(() => {
+    setDateToPayment(datesService.daysToPayment())
+  }, [])
+
+  useEffect(() => {
+    setMonthName(datesService.monthName());
+  },[])
+
   return (
     <>
       <Header />
       
-      <Container>
+      <Container className="mt-4">
         <Row>
           <Col>
-          <Jumbotron>
-            <h1>Gerenciador de Alugueis</h1>
-            <p>
-              Gerencie os seus alugueis e mantenha em dia todo os pagamentos dos seus inquilinos
-            </p>
-            <p>
-              <Button variant="primary">Learn more</Button>
-            </p>
-          </Jumbotron>
+            <Jumbotron>
+              <h1>Gerenciador de Alugueis</h1>
+              <p>
+                Gerencie os seus alugueis e mantenha em dia todo os pagamentos dos seus inquilinos
+              </p>
+              <p>
+                <Button variant="primary">Learn more</Button>
+              </p>
+            </Jumbotron>
           </Col>
         </Row>
+
+        <Row>
+          <Col md={4}>
+            <InfoCard title={monthName} bg="info" content="Mês atual"/>
+          </Col>
+          <Col md={4}>
+            <InfoCard title={`${dateToPayment} dias`} bg={ dateToPayment < 5 ? 'success' : 'warning' } content="Para receber o proximo aluguel"/>
+          </Col>
+        </Row>
+        
       </Container>
     </>
   )

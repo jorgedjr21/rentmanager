4cf0cfa21876cee507e1ab981f8ea2e6285df248
Jorge Junior <jorgedjr21@gmail.com>
2020-06-06 23:27:36 -0300

    Main react little adjustments

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import {Button} from 'react-bootstrap'
 import 'bootswatch/dist/materia/bootstrap.min.css'
+import './index.css'
 
 import App from './App';
 
